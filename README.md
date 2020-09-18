3dee1fc01729002d463d6a5681a33faeee322595
Hopetree <zlwork2014@163.com>
2020-09-18 23:18:28 +0800

    v1.0.0 push to pypi

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # py2curl
-python requests to curl
+
+将 python 的 requests 代码转换成 curl 命令
+
+## 安装
+
+```bash
+pip install py2curl
+```
+
+## 使用
+
+```python
+import requests
+import py2curl
+
+req = requests.get('https://tendcode.com')
+result = py2curl.render(req.request)
+print(result)
+### curl -k -v -X GET -H "Accept: */*" -H "Accept-Encoding: gzip, deflate" -H "Connection: keep-alive" -H "User-Agent: python-requests/2.19.1" https://tendcode.com/
+```
---
+++ setup.py
@@ -0,0 +1,32 @@
+# -*- coding:utf-8 -*-
+# Author: https://github.com/Hopetree
+# Date: 2020/7/11
+from setuptools import find_packages, setup
+
+VERSION = '1.0.0'
+
+with open('README.md', 'r', encoding='utf-8') as fp:
+    long_description = fp.read()
+
+setup(
+    name='py2curl',
+    version=VERSION,
+    py_modules=[
+        'py2curl',
+    ],
+    author='Hopetree',
+    author_email='zlwork2014@163.com',
+    description='A tool render requests object to curl',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/Hopetree/py2curl',
+    keywords='python requests curl',
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+    ],
+)
